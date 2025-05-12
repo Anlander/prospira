597ca619350903260031443780ce8d8d8d5fb737
anlander <anlanderp@gmail.com>
2025-05-12 17:45:35 +0200

    footer

--- src/components/ui/footer/footer-section.tsx
+++ src/components/ui/footer/footer-section.tsx
@@ -3,7 +3,7 @@ import { Socials } from "../socials/socials";
 
 export const FooterSection = ({ props }: any) => {
   return (
-    <footer className="flex flex-col gap-5 justify-center items-center min-h-[30vh]" style={{ background: `${props.bg_footer?.color}` }}>
+    <footer className="flex flex-col gap-5 justify-center items-center min-h-[40vh]" style={{ background: `${props.bg_footer?.color}` }}>
       <div
         className={`flex h-full justify-center mx-auto text-center lg:text-left pb-10 lg:pb-10  pt-4 relative ${props.footer_full_width ? "w-full" : "container-section"
           }`}
@@ -14,7 +14,10 @@ export const FooterSection = ({ props }: any) => {
         </div>
       </div>
       <p className="text-center">Prospiro AB <br />
-        orgnr: 559522-4964</p>
+        070-536 665 50
+        <br />
+        orgnr: 559522-4964
+      </p>
       <p>Copyrights Prospiro 2025 ©</p>
     </footer>
   );
